ce8fdb61f0681a6c8e3cfff4891195141eaaadf7
Rif <Rif@MacBook-Air-Rif.local>
2025-05-23 12:56:11 +0300

    upgrade

--- remove_meta.php
+++ remove_meta.php
@@ -1,25 +1,69 @@
 <?php
 
-$html = file_get_contents('index.html'); 
+class MetaCleaner implements Iterator
+{
+    private array $lines;
+    private array $namesToRemove;
+    private int $position = 0;
+    private array $removedTags = [];
 
-preg_match_all('/<meta\b[^>]*>/i', $html, $matches);
+    public function __construct(array $lines, array $namesToRemove)
+    {
+        $this->lines = $lines;
+        $this->namesToRemove = $namesToRemove;
+    }
 
-$metaTags = $matches[0];
+    public function current(): string
+    {
+        $line = $this->lines[$this->position];
 
-$namesToRemove = ['title', 'description', 'keywords'];
-$removedTags = [];
-
-foreach ($metaTags as $metaTag) {
-
-    foreach ($namesToRemove as $name) {
-        if (preg_match('/name=["\']?' . preg_quote($name, '/') . '["\']?/i', $metaTag)) {
-
-            $removedTags[] = $metaTag;
-
-            $html = str_replace($metaTag, '', $html);
-            break; 
+        if (preg_match_all('/<meta\b[^>]*>/i', $line, $matches)) {
+            foreach ($matches[0] as $metaTag) {
+                foreach ($this->namesToRemove as $name) {
+                    if (preg_match('/name=["\']?' . preg_quote($name, '/') . '["\']?/i', $metaTag)) {
+                        $this->removedTags[] = $metaTag;
+                        $line = str_replace($metaTag, '', $line);
+                        break;
+                    }
+                }
+            }
         }
+
+        return $line;
+    }
+
+    public function key(): int
+    {
+        return $this->position;
+    }
+
+    public function next(): void
+    {
+        $this->position++;
+    }
+
+    public function rewind(): void
+    {
+        $this->position = 0;
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->lines[$this->position]);
+    }
+
+    public function getRemovedTags(): array
+    {
+        return $this->removedTags;
     }
 }
 
+$lines = file('index.html');
+$namesToRemove = ['title', 'description', 'keywords'];
 
+$cleaner = new MetaCleaner($lines, $namesToRemove);
+
+$cleanedHtml = '';
+foreach ($cleaner as $line) {
+    $cleanedHtml .= $line;
+}
